bdde4f9fd1743f8965ee26b3510c8673ea4b4aa1
Javier Rivera Caviedes <javierriveracaviedes@MacBook-Pro-de-Javier.local>
2020-06-01 06:31:38 -0400

    actualizacion de configuracion para despliegue en Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA85cyTeJfJ_ua_OUX5aNo1-GLTEtNUHlA",
+    authDomain: "desafiotecnico2brains.firebaseapp.com",
+    databaseURL: "https://desafiotecnico2brains.firebaseio.com",
+    projectId: "desafiotecnico2brains",
+    storageBucket: "desafiotecnico2brains.appspot.com",
+    messagingSenderId: "545103008261",
+    appId: "1:545103008261:web:fb43efd33dbd355c0fc26c"
+  }
 };
